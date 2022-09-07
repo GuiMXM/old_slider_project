2c0a4f913e957321541bcf19ac6cc032d6ca613f
Germano <germano-software-dev@hotmail.com>
2022-09-07 16:15:45 -0300

    feat: Add user comments to code explanation.

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -9,17 +9,24 @@ import { firstValueFrom } from "rxjs";
 })
 export class SliderComponent implements OnInit {
   base64:string = "data:image/png;base64, ";
-  id: string = '0000000000000001';
+  api_url: string = "https://swordcraft-image-generator.herokuapp.com/create-image/";
+  id: string = "0000000000000001";
   images: string[] = [];
 
   constructor(private http: HttpClient) {}
   
   async ngOnInit(): Promise<void> {
+    // for loop to get inital 3 images
     for(let i = 0; i < 3; i++) {
+      // http request for the api url
       const req = this.http
-      .get('https://swordcraft-image-generator.herokuapp.com/create-image/'+ this.id)
+      // this.id is fixed but in near future with image API updates it could be dynamic too
+      .get(this.api_url+this.id);
+
       // TODO: type this into Data Transfer Object (DTO)
       const res: any = await firstValueFrom(req);
+      
+      // add the encoded base64 image in the array
       this.images.push(this.base64+res.raw_image);
     }
     // console.log(this.images);
